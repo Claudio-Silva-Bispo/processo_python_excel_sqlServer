bf7f560596077e89a5293c935d9a43318d4f8351
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-10 16:31:23 -0300

    Create autenticacao_banco.py

---
+++ grade-comercial/autenticacao_banco.py
@@ -0,0 +1,19 @@
+# Configurações de conexão com o banco de dados SQL Server
+DB_CONFIG = {
+    "dw_producao" : {
+    'driver': '{ODBC Driver 17 for SQL Server}', 
+    'servidor': '',
+    'banco': '',
+    'usuario': '',
+    'senha': ''},        
+
+    "gestao_comercial" : {   
+    'driver': '{ODBC Driver 17 for SQL Server}', 
+    'servidor': '',
+    'banco': '',
+    'usuario': '',
+    'senha': ''
+    }
+}
+
+
