1bad1455fa607c788a2368210302a4d672a9d56a
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-11 15:03:52 -0300

    Create autenticacao_banco.py

---
+++ estrutura-comercial/autenticacao_banco.py
@@ -0,0 +1,21 @@
+# Arquivo que terá minhas credenciais do banco para acesso.
+
+# Configurações de conexão com o banco de dados SQL Server
+DB_CONFIG = {
+    "dw_producao" : {
+    'driver': '{ODBC Driver 17 for SQL Server}', 
+    'servidor': '',
+    'banco': '',
+    'usuario': '',
+    'senha': ''},        
+
+    "gestao_comercial" : {   
+    'driver': '{ODBC Driver 17 for SQL Server}', 
+    'servidor': '',
+    'banco': '',
+    'usuario': '',
+    'senha': ''
+    }
+}
+
+
