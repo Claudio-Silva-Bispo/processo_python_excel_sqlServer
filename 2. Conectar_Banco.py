54fe2de138eacba56935361eee8acf5e4adb2bff
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-11 16:20:56 -0300

    Update 2. Conectar_Banco.py

--- 2. Conectar_Banco.py
+++ 2. Conectar_Banco.py
@@ -1,29 +1,10 @@
-# Com base na minha função DB_CONFIG, vou coletar as credenciais, para não deixar exposta no script.
-def conectar_banco():
-    conexao_str = (
-        f"DRIVER={DB_CONFIG['driver']};"
-        f"SERVER={DB_CONFIG['servidor']};"
-        f"DATABASE={DB_CONFIG['banco']};"
-        f"UID={DB_CONFIG['usuario']};"
-        f"PWD={DB_CONFIG['senha']}"
-    )
-    conexao = pyodbc.connect(conexao_str)
-    return conexao
-
-def executar_consulta(query, params=None):
-    conexao = conectar_banco()
-    cursor = conexao.cursor()
-    try:
-        if params:
-            cursor.execute(query, params)
-        else:
-            cursor.execute(query)
-        
-        resultados = cursor.fetchall()
-        return resultados if resultados else [] 
-    except Exception as e:
-        print(f"Erro ao executar a consulta: {e}")
-        return [] 
-    finally:
-        cursor.close()
-        conexao.close()
+def conectar(self, nome_tabela: Literal['nome', 'nome_dois']) -> pyodbc.Connection:
+        config = self.db_config[nome_tabela]
+        conexao_str = (
+            f"DRIVER={config['driver']};"
+            f"SERVER={config['servidor']};"
+            f"DATABASE={config['banco']};"
+            f"UID={config['usuario']};"
+            f"PWD={config['senha']}"
+        )
+        return pyodbc.connect(conexao_str)
