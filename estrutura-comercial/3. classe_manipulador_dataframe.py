089e77783fe3b2dd63986839727e5fb6d3e1f38f
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-12 09:24:37 -0300

    Criação da classe que será responsável por manipular o arquivo, se for necessário.
    Neste primeiro momento, como não estamos tratando nada, essa classe vai apenas encaminha o arquivo para próxima, mas se for necessário criar qualquer modificação, ela já está pronta para ser utilizada.

---
+++ estrutura-comercial/3. classe_manipulador_dataframe.py
@@ -0,0 +1,10 @@
+class ManipuladorDataFrame:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def exibir(self):
+        print(self.dataframe)
+
+    def manipular(self):
+        # Adicione aqui as funcionalidades de manipulação - Se precisar fazer mais alguma coisa. Por agora não precisa.
+        pass
