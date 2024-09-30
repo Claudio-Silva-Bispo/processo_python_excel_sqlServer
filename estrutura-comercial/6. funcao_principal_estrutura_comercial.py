19e262c715a039edeb465066fe2477e7be2e65c7
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-30 12:14:14 -0300

    Alteração do fluxo de envio da base consolidada.

--- estrutura-comercial/6. funcao_principal_estrutura_comercial.py
+++ estrutura-comercial/6. funcao_principal_estrutura_comercial.py
@@ -1,15 +1,19 @@
 
+from datetime import datetime
+import sys
 import BancoDadosEstruturaComercial
 import ProcessadorExcel
 import ManipuladorDataFrameEstruturaComercial
 
 import pandas as pd
 
-# Passar aqui caminho onde os arquivos ficarão armazenados.
-caminho_pasta = fr'inserir caminho aqui'
-caminho_destino = fr'inserir caminho aqui'
+import EnviarEmail
+import script_html
 
-# Chamar a função principal ai de cima para executar a separação de cada aba em arquivos dataframe
+# Passar aqui caminho onde os arquivos ficarão armazenados.
+caminho_pasta = fr''
+caminho_destino = fr''
+
 processador = ProcessadorExcel.ProcessadorExcel(caminho_pasta, caminho_destino)
 
 # Tratativa e separação dos arquivos. Objetivo aqui é saber se deu certo.
@@ -19,9 +23,18 @@ try:
         #print(f"Manipulando a aba: {nome_aba}")
         manipulador = ManipuladorDataFrameEstruturaComercial.ManipuladorDataFrame(df)
 except FileNotFoundError as e:
-    print(e)
 
-def funcao_principal():
+        assunto = 'Verificar processo de atualização da ESTRUTURA COMERCIAL.'
+        destinatario =script_html.suporte_email.get('dados_falha_estrutura').get('destinatario')
+        copias = script_html.suporte_email.get('dados_falha_estrutura').get('copias')
+        corpo = 'Processo foi interrompido na etapa inicial de procurar o arquivo.'
+        corpo_html = script_html.html_falha
+
+        EnviarEmail.enviar_email(assunto=assunto, destinatario=destinatario, copias=copias, corpo=corpo, corpo_html=corpo_html, arquivo_anexo='',caminho_anexo_xlsx='', nome_arquivo_xlsx='')
+
+        sys.exit(f"Não foi possível encontrar o arquivo na pasta {caminho_pasta}")
+
+def funcao_principal_estrutura():
 
     # Inserir os dados conforme a lista de tabelas e seus dataframes.
     tabelas_e_dataframes = [
@@ -39,22 +52,51 @@ def funcao_principal():
 
     # Insira os dados conforme a lista de tabelas e seus dataframes.
     for nome_tabela, dataframe in tabelas_e_dataframes:
+        agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
         if dataframe is not None: 
+            dataframe['DATA_ATUALIZACAO'] = agora
             nome_conexao = "gestao_comercial"
             inserir_dados = BancoDadosEstruturaComercial.BancoDados(dataframe, nome_tabela, nome_conexao)
             inserir_dados.inserir()
             print(f"Dados inseridos na tabela: {nome_tabela}")
         else:
             print(f"DataFrame para a tabela {nome_tabela} não encontrado.")
+            assunto = f'Verificar processo de atualização da ESTRUTURA COMERCIAL. Não foi possível inserir as informações no banco/n. DataFrame para a tabela {nome_tabela} não encontrado.'
+            destinatario =script_html.suporte_email.get('dados_falha_estrutura').get('destinatario')
+            copias = script_html.suporte_email.get('dados_falha_estrutura').get('copias')
+            corpo = 'Processo foi interrompido na etapa inicial de procurar o arquivo.'
+            corpo_html = script_html.html_falha
+
+            EnviarEmail.enviar_email(assunto=assunto, destinatario=destinatario, copias=copias, corpo=corpo, corpo_html=corpo_html, arquivo_anexo='',caminho_anexo_xlsx='', nome_arquivo_xlsx='')
 
     # Enviar somente a base consolidada
     base_consolidada = dataframes.get('BASE_CONSOLIDADA')
-    base_consolidada['DATA_BASE'] = pd.to_datetime(base_consolidada['DATA_BASE'], errors='coerce')
-    inserir_dados = BancoDadosEstruturaComercial.BancoDados(dataframe, nome_tabela, nome_conexao)
+    base_consolidada
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    base_consolidada['DATA_ATUALIZACAO'] = agora
+
+    if any(base_consolidada.columns.str.startswith('Unnamed')):
+        base_consolidada = base_consolidada.loc[:, ~base_consolidada.columns.str.startswith('Unnamed')]
+
+    try:
+        inserir_dados = BancoDadosEstruturaComercial.BancoDados(dataframe=base_consolidada, nome_tabela='METAS', nome_conexao='nome_aqui')
+        inserir_dados.inserir()
+    except Exception as e:
+        print(f"Erro ao inserir dados na tabela METAS: {e}")
 
     limpar = ProcessadorExcel.ProcessadorExcel(caminho_pasta='', caminho_destino=caminho_destino)
     limpar.limpar_pasta_destino()
     print("Processo para criar a estrutura da Gestão Comercial finalizou.")
 
-#funcao_principal() # se for para rodar só ela, descomente aqui. Caso contrário, ela será chamada no processo principal global.
-print("Processo para criar a estrutura da Gestão Comercial finalizou.")
+    assunto = script_html.suporte_email.get('dados_sucesso_estrutura').get('assunto')
+    destinatario =script_html.suporte_email.get('dados_sucesso_estrutura').get('destinatario')
+    copias = script_html.suporte_email.get('dados_sucesso_estrutura').get('copias')
+    corpo = 'Processo para criar a ESTRUTURA da Gestão Comercial finalizou.'
+    corpo_html = script_html.html_sucesso_estrutura
+
+    EnviarEmail.enviar_email(assunto=assunto, destinatario=destinatario, copias=copias, corpo=corpo, corpo_html=corpo_html,caminho_anexo_xlsx=None, nome_arquivo_xlsx='')
+
+    print("E-mail enviado para todos sinalizando sobre a finalização do processo.")
+
+funcao_principal_estrutura()
