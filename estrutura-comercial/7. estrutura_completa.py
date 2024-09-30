7e74ca0a1b43ba7726ad43aca5653d7dcc41fb8e
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-30 12:16:03 -0300

    Update 7. estrutura_completa.py

--- estrutura-comercial/7. estrutura_completa.py
+++ estrutura-comercial/7. estrutura_completa.py
@@ -182,7 +182,7 @@ class BancoDados:
             cursor.close()
             conexao.close()
 
-def funcao_principal():
+def funcao_principal_estrutura():
 
     # Inserir os dados conforme a lista de tabelas e seus dataframes.
     tabelas_e_dataframes = [
@@ -200,21 +200,52 @@ def funcao_principal():
 
     # Insira os dados conforme a lista de tabelas e seus dataframes.
     for nome_tabela, dataframe in tabelas_e_dataframes:
+        agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
         if dataframe is not None: 
-            nome_conexao = "gestao_comercial"
-            inserir_dados = BancoDados(dataframe, nome_tabela, nome_conexao)
+            dataframe['DATA_ATUALIZACAO'] = agora
+            nome_conexao = "nome_aqui"
+            inserir_dados = BancoDadosEstruturaComercial.BancoDados(dataframe, nome_tabela, nome_conexao)
             inserir_dados.inserir()
-            #print(f"Dados inseridos na tabela: {nome_tabela}")
-        #else:
-            #print(f"DataFrame para a tabela {nome_tabela} não encontrado.")
+            print(f"Dados inseridos na tabela: {nome_tabela}")
+        else:
+            print(f"DataFrame para a tabela {nome_tabela} não encontrado.")
+            assunto = f'Verificar processo de atualização da ESTRUTURA COMERCIAL. Não foi possível inserir as informações no banco/n. DataFrame para a tabela {nome_tabela} não encontrado.'
+            destinatario =script_html.suporte_email.get('dados_falha_estrutura').get('destinatario')
+            copias = script_html.suporte_email.get('dados_falha_estrutura').get('copias')
+            corpo = 'Processo foi interrompido na etapa inicial de procurar o arquivo.'
+            corpo_html = script_html.html_falha
+
+            EnviarEmail.enviar_email(assunto=assunto, destinatario=destinatario, copias=copias, corpo=corpo, corpo_html=corpo_html, arquivo_anexo='',caminho_anexo_xlsx='', nome_arquivo_xlsx='')
 
     # Enviar somente a base consolidada
     base_consolidada = dataframes.get('BASE_CONSOLIDADA')
-    base_consolidada['DATA_BASE'] = pd.to_datetime(base_consolidada['DATA_BASE'], errors='coerce')
-    inserir_dados = BancoDados(dataframe, nome_tabela, nome_conexao)
+    base_consolidada
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    base_consolidada['DATA_ATUALIZACAO'] = agora
 
-    limpar = ProcessadorExcel(caminho_pasta='', caminho_destino=caminho_destino)
+    if any(base_consolidada.columns.str.startswith('Unnamed')):
+        base_consolidada = base_consolidada.loc[:, ~base_consolidada.columns.str.startswith('Unnamed')]
+
+    try:
+        inserir_dados = BancoDadosEstruturaComercial.BancoDados(dataframe=base_consolidada, nome_tabela='METAS', nome_conexao='nome_aqui')
+        inserir_dados.inserir()
+    except Exception as e:
+        print(f"Erro ao inserir dados na tabela METAS: {e}")
+
+    limpar = ProcessadorExcel.ProcessadorExcel(caminho_pasta='', caminho_destino=caminho_destino)
     limpar.limpar_pasta_destino()
+    print("Processo para criar a estrutura da Gestão Comercial finalizou.")
 
-funcao_principal
+    assunto = script_html.suporte_email.get('dados_sucesso_estrutura').get('assunto')
+    destinatario =script_html.suporte_email.get('dados_sucesso_estrutura').get('destinatario')
+    copias = script_html.suporte_email.get('dados_sucesso_estrutura').get('copias')
+    corpo = 'Processo para criar a ESTRUTURA da Gestão Comercial finalizou.'
+    corpo_html = script_html.html_sucesso_estrutura
+
+    EnviarEmail.enviar_email(assunto=assunto, destinatario=destinatario, copias=copias, corpo=corpo, corpo_html=corpo_html,caminho_anexo_xlsx=None, nome_arquivo_xlsx='')
+
+    print("E-mail enviado para todos sinalizando sobre a finalização do processo.")
+
+funcao_principal_estrutura()
 print("Processo para criar a estrutura da Gestão Comercial finalizou.")
